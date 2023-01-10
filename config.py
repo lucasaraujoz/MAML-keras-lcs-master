8bdf1006b2b0f1c9e93e8fb2de6e324be95f02f1
lucas <lcsaraujo88@gmail.com>
2023-01-09 21:38:18 -0300

    last changes

--- config.py
+++ config.py
@@ -18,22 +18,22 @@ parser.add_argument('--summary_path', type=str,
                     default="./summary",
                     help='The directory of the summary writer.')
 
-parser.add_argument('--batch_size', type=int, default=32,
+parser.add_argument('--batch_size', type=int, default=3,
                     help='Number of task per train batch.')
 parser.add_argument('--val_batch_size', type=int, default=16,
                     help='Number of task per test batch.')
-parser.add_argument('--epochs', type=int, default=100,
+parser.add_argument('--epochs', type=int, default=4,
                     help='The training epochs.')
 parser.add_argument('--inner_lr', type=float, default=0.0001,
                     help='The learning rate of of the support set.')
 parser.add_argument('--outer_lr', type=float, default=0.001,
                     help='The learning rate of of the query set.')
 
-parser.add_argument('--n_way', type=int, default=10,
+parser.add_argument('--n_way', type=int, default=2,
                     help='The number of class of every task.')
-parser.add_argument('--k_shot', type=int, default=1,
+parser.add_argument('--k_shot', type=int, default=3,
                     help='The number of support set image for every task.')
-parser.add_argument('--q_query', type=int, default=1,
+parser.add_argument('--q_query', type=int, default=3,
                     help='The number of query set image for every task.')
 parser.add_argument('--input_shape', type=tuple, default=(128, 128, 3),
                     help='The image shape of model input.')
